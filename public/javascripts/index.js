1c260ef03434d7c5dbf5deb1ff7a78bf04cd380f
wudi <wudi@woodydeMac-mini.local>
2016-08-02 11:39:05 +0800

    第二次提交 修改了某些样式

--- public/javascripts/index.js
+++ public/javascripts/index.js
@@ -1,24 +1,5 @@
 $(document).ready(function() {
 
-  var canvas = document.getElementById('canvas')
-  var context = canvas.getContext('2d')
-
-  canvas.width = window.innerWidth
-  canvas.height = window.innerHeight
-
-  var image = new Image()
-  image.src = "images/bg.jpg"
-
-  image.onload = function(e){
-    draw( image )
-  }
-
-  function draw(image){
-    context.clearRect(0, 0, canvas.width, canvas.height)
-    context.save()
-    context.drawImage( image, 0, 0,canvas.width, canvas.height)
-    context.restore()
-  }
 
   $(".js-example-basic-single").select2({
   	minimumResultsForSearch: Infinity,
